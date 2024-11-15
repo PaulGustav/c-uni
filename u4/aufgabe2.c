6c00d74a03a3733f9ef42708e0adc87f6d10b55a
janis <diabolo2110@gmail.com>
2024-11-16 00:27:04 +0100

    aufgabe 3 ü4

--- u4/aufgabe2.c
+++ u4/aufgabe2.c
@@ -1,14 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-	int numbers[5] = {1, 2, 3, 4, 5};
-	char letters[4] = {'a', 'b', 'c', 'd'};
+	char start_letter, end_letter;
+	int start_digit, end_digit;
 
-	for (int i=0; i < 4; i++) {
-		for (int n=0; n < 4; n++) {
-			printf("(%c, %d),\n", letters[i], numbers[n]);
-		}
+	printf("Bitte Anfangsbuchstabe eingeben: ");
+	scanf("%c", &start_letter);
+	getchar();
+	printf("Bitte Endbuchstaben eingeben: ");
+	scanf("%c", &end_letter);
+	getchar();
+
+	printf("Bitte Anfangszahl eingeben: ");
+	scanf("%d", &start_digit);
+	printf("Bitte Endzahl eingeben: ");
+	scanf("%d", &end_digit);
+
+	int letter_count = end_letter - start_letter + 1;
+	int digit_count = end_digit - start_digit + 1;
+
+	char letters[letter_count];
+	int numbers[digit_count];
+
+	if (end_digit < start_digit) {
+		fprintf(stderr, "Fehler: Endzahl ist kleiner als Startzahl!\n");
+		exit(EXIT_FAILURE);
 	}
 
-	return 0;
+	for (int n = 0; n < letter_count; n++) {
+		letters[n] = start_letter + n;
+	}
+
+	for (int n = 0; n < digit_count; n++) {
+		numbers[n] = start_digit + n;
+	}
+
+	for (int i=0; i < letter_count; i++) {
+		for (int n=0; n < digit_count; n++) {
+			printf("(%c, %d) ", letters[i], numbers[n]);
+		}
+
+		printf("\n");
+	}
+
+	return (EXIT_SUCCESS);
 }
