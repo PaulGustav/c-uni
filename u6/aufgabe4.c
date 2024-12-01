4c6e6a42caae22e53e58fc387bb8ad2a6d99f289
janis <diabolo2110@gmail.com>
2024-12-01 13:43:25 +0100

    a4 immernoch

--- u6/aufgabe4.c
+++ u6/aufgabe4.c
@@ -2,18 +2,26 @@
 #include <string.h>
 #include <stdbool.h>
 
-bool is_palindrom(char input[]) {
-	size_t size = sizeof(input);
+bool is_palindrom(char input[], size_t size) {
+	char lower[size];
+	
+	for (int i = 0; input[i] != '\0'; i++) {
+		lower[i] = input[i] + 32;
+	}
 
+	lower[size] = '\0';
 	
 }
 
 int main() {
 	char input[255];
+	size_t size = sizeof(input);
 
 	printf("Bitte Text eingeben: ");
-	fgets(input, sizeof(input), stdin);
+	fgets(input, size, stdin);
 	input[strcspn(input, "\n")] = '\0';
+
+	is_palindrom(input, size);
 
 	return 0;
 }
