05ae5512494902595839037e056132446aa4f275
janis <diabolo2110@gmail.com>
2024-12-01 14:53:15 +0100

    a4 komplett

--- u6/aufgabe4.c
+++ u6/aufgabe4.c
@@ -1,15 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
-bool is_palindrom(char input[], size_t size) {
-	char lower[size];
+bool is_palindrom(char input[]) {
+	int length = strlen(input);
+
+	char lower[length + 1];
+	char turned[length + 1];
+
 	
 	for (int i = 0; input[i] != '\0'; i++) {
-		lower[i] = input[i] + 32;
+		if (input[i] > 40 && input[i] < 91) {
+			lower[i] = input[i] + 32;
+		} else {
+			lower[i] = input[i];
+		}
 	}
 
-	lower[size] = '\0';
+	lower[length] = '\0';
+
+	for (int i = 0; lower[i] != '\0'; i++) {
+		turned[i] = lower[length - i - 1];
+	}
+	
+	turned[length] = '\0';
+
+	for (int i = 0; i < length; i++) {
+		if (turned[i] != lower[i]) {
+			return false;
+		}
+	}
+
+	return true;
 	
 }
 
@@ -21,7 +44,12 @@ int main() {
 	fgets(input, size, stdin);
 	input[strcspn(input, "\n")] = '\0';
 
-	is_palindrom(input, size);
+	if (is_palindrom(input)) {
+		printf("'%s' ist ein Palindrom.\n", input);
+	} else {
+		fprintf(stderr, "'%s' ist kein Palindrom.\n", input);
+		exit(EXIT_FAILURE);
+	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }
